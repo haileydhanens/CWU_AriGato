fad86ee7539f81c91bec3950fe43843c00644843
Angie <37876535+angelanquach@users.noreply.github.com>
2019-01-22 13:57:51 -0800

    FacialRecognition
    Figured out how to get NAO to recognize my face by using the "Learn Face" module. I've set up another behavior in this project that says hi to you once NAO has learned your face.

---
+++ InProgress/FacialRecognition/translations/translation_en_US.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>nope not angie</source>
+            <comment>Text</comment>
+            <translation type="obsolete">nope not angie</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Angie</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Angie</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Hailey</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Hailey</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello!</translation>
+        </message>
+        <message>
+            <source>Hello Angie</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Angie</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
