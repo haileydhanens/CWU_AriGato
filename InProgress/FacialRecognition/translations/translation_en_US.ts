5b0786d1af85fd28fb7980b6dc0e83f935cb7f90
Angie <37876535+angelanquach@users.noreply.github.com>
2019-01-28 15:18:22 -0800

    todays work

--- InProgress/FacialRecognition/translations/translation_en_US.ts
+++ InProgress/FacialRecognition/translations/translation_en_US.ts
@@ -59,10 +59,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Animated Say (5)</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>I don't think we've met before.</source>
             <comment>Text</comment>
-            <translation type="unfinished">I don't think we've met before.</translation>
+            <translation type="obsolete">I don't think we've met before.</translation>
         </message>
     </context>
     <context>
