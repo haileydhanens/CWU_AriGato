062ba2cc271404c3b3a98ba11f018579b94d5ee0
Angie <37876535+angelanquach@users.noreply.github.com>
2019-01-28 14:41:36 -0800

    Facial Recognition
    Facial recognition module now has a default case for someone whose face it hasn't learned yet.

--- InProgress/FacialRecognition/translations/translation_en_US.ts
+++ InProgress/FacialRecognition/translations/translation_en_US.ts
@@ -30,6 +30,42 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Casey</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Casey</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Derek</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Derek</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Matt</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Matt</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I don't think we've met before.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I don't think we've met before.</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
