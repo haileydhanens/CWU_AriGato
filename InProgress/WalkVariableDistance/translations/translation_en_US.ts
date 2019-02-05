44ef7528712438b1fc2eac1efd749e76de541383
MattSHarker <mattsharker@gmail.com>
2019-02-05 10:13:00 -0800

    Modified File Structure
    Instead of trying to get all the info from a single command, the program now asks the user three questions.

--- InProgress/WalkVariableDistance/translations/translation_en_US.ts
+++ InProgress/WalkVariableDistance/translations/translation_en_US.ts
@@ -18,10 +18,15 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>How far do you want me to walk?</source>
             <comment>Text</comment>
-            <translation type="unfinished">How far do you want me to walk?</translation>
+            <translation type="obsolete">How far do you want me to walk?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Are we using feet or inches?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Are we using feet or inches?</translation>
         </message>
     </context>
     <context>
@@ -35,6 +40,12 @@
             <source>What direction do you want me to walk?</source>
             <comment>Text</comment>
             <translation type="obsolete">What direction do you want me to walk?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>How far do you want me to walk?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">How far do you want me to walk?</translation>
         </message>
     </context>
 </TS>
