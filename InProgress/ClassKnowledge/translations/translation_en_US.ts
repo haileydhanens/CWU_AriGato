e51f7fb059862e57fb5bb93fc83d357a3eaeb688
Derek Vaughan <34034665+derek-vaughan@users.noreply.github.com>
2019-02-20 15:35:20 -0800

    Added more CS classes
    I've added more classes to the ClassKnowledge module. The switch statement is already starting to get extremely long. I think we should do some testing before adding the rest of the core classes to make sure everything works okay.

--- InProgress/ClassKnowledge/translations/translation_en_US.ts
+++ InProgress/ClassKnowledge/translations/translation_en_US.ts
@@ -17,14 +17,79 @@
             <comment>Text</comment>
             <translation type="obsolete">C S one eleven. I remember learning about classes and recursion in that class.</translation>
         </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh! C S one eleven. That was the second C S class that I took. I learned about recursion and other fun topics.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh! C S one eleven. That was the second C S class that I took. I learned about recursion and other fun topics.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS112</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>In C S one twelve I learned about robots. That was sort of creepy!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">In C S one twelve I learned about robots. That was sort of creepy!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS250</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>In C S two fifty I learned the basics of web development. I love H T M L!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">In C S two fifty I learned the basics of web development. I love H T M L!</translation>
+        </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/CS301</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>I loved data structures</source>
             <comment>Text</comment>
-            <translation type="unfinished">I loved data structures</translation>
+            <translation type="obsolete">I loved data structures</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>C S 3 0 1 was great! Data structures are super important.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">C S 3 0 1 was great! Data structures are super important.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS302</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>C S 3 0 2 helped me learn a lot more about trees, searching, and sorting. Those labs were tough!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">C S 3 0 2 helped me learn a lot more about trees, searching, and sorting. Those labs were tough!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS311</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>three eleven was a tough class. You will learn all about computer architecture and the assembly language!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">three eleven was a tough class. You will learn all about computer architecture and the assembly language!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS312</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>C S three twelve was very interesting. You will learn about topics like digital circuits, memory, and central processing units.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">C S three twelve was very interesting. You will learn about topics like digital circuits, memory, and central processing units.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS325</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Three twenty five helped me be able to write some great technical papers.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Three twenty five helped me be able to write some great technical papers.</translation>
         </message>
     </context>
     <context>
