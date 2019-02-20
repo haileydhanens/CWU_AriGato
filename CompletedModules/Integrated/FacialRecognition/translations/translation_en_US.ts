a20d9f9a088b9ec617270ebd6f95c3217e374cd4
Matt Harker <mattsharker@gmail.com>
2019-02-20 12:32:13 -0800

    Brought fork up to date
    The fork will now be up to date as of 2019/02/20

---
+++ CompletedModules/Integrated/FacialRecognition/translations/translation_en_US.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>nope not angie</source>
+            <comment>Text</comment>
+            <translation type="obsolete">nope not angie</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Angie</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Angie</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Hailey</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Hailey</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Casey</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Casey</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Derek</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Derek</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Matt</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Matt</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <source>I don't think we've met before.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I don't think we've met before.</translation>
+        </message>
+        <message>
+            <source>I do not recognise you</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I do not recognise you</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello!</translation>
+        </message>
+        <message>
+            <source>Hello Angie</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Angie</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
---
+++ InProgress/ClassKnowledge/translations/translation_en_US.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/CS110</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh! C S one ten. That was there I learned all the basics. Good times back then.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh! C S one ten. That was there I learned all the basics. Good times back then.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS111</name>
+        <message>
+            <source>C S one eleven. I remember learning about classes and recursion in that class.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">C S one eleven. I remember learning about classes and recursion in that class.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS301</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I loved data structures</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I loved data structures</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I didn't quite catch that. Please say the class again.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I didn't quite catch that. Please say the class again.</translation>
+        </message>
+    </context>
+</TS>
