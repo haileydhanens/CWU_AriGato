f3bdd624bda09093f154308cbb8d7673cce9fe59
Matt Harker <mattsharker@gmail.com>
2019-02-22 15:12:13 -0800

    First working version of Class Knowledge
    This is the first working version of Class Knowledge. It will ask the user to name a class (can be the number or the title) and will give a description of the class.
    Currently, the classes need updated descriptions to make them easier to understand.

--- InProgress/ClassKnowledge/translations/translation_en_US.ts
+++ InProgress/ClassKnowledge/translations/translation_en_US.ts
@@ -2,12 +2,20 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
-        <name>behavior_1/behavior.xar:/CS110</name>
+        <name>behavior_1/behavior.xar:/Ask again</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>What other class would you like to know about?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What other class would you like to know about?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/CS110</name>
+        <message>
             <source>Oh! C S one ten. That was there I learned all the basics. Good times back then.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Oh! C S one ten. That was there I learned all the basics. Good times back then.</translation>
+            <translation type="obsolete">Oh! C S one ten. That was there I learned all the basics. Good times back then.</translation>
         </message>
     </context>
     <context>
@@ -17,30 +25,6 @@
             <comment>Text</comment>
             <translation type="obsolete">C S one eleven. I remember learning about classes and recursion in that class.</translation>
         </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Oh! C S one eleven. That was the second C S class that I took. I learned about recursion and other fun topics.</source>
-            <comment>Text</comment>
-            <translation type="unfinished">Oh! C S one eleven. That was the second C S class that I took. I learned about recursion and other fun topics.</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/CS112</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>In C S one twelve I learned about robots. That was sort of creepy!</source>
-            <comment>Text</comment>
-            <translation type="unfinished">In C S one twelve I learned about robots. That was sort of creepy!</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/CS250</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>In C S two fifty I learned the basics of web development. I love H T M L!</source>
-            <comment>Text</comment>
-            <translation type="unfinished">In C S two fifty I learned the basics of web development. I love H T M L!</translation>
-        </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/CS301</name>
@@ -49,47 +33,18 @@
             <comment>Text</comment>
             <translation type="obsolete">I loved data structures</translation>
         </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>C S 3 0 1 was great! Data structures are super important.</source>
-            <comment>Text</comment>
-            <translation type="unfinished">C S 3 0 1 was great! Data structures are super important.</translation>
-        </message>
     </context>
     <context>
-        <name>behavior_1/behavior.xar:/CS302</name>
+        <name>behavior_1/behavior.xar:/Exit speech</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>C S 3 0 2 helped me learn a lot more about trees, searching, and sorting. Those labs were tough!</source>
+            <source>Thank you for asking about our classes! Have a nice day</source>
             <comment>Text</comment>
-            <translation type="unfinished">C S 3 0 2 helped me learn a lot more about trees, searching, and sorting. Those labs were tough!</translation>
+            <translation type="obsolete">Thank you for asking about our classes! Have a nice day</translation>
         </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/CS311</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>three eleven was a tough class. You will learn all about computer architecture and the assembly language!</source>
+            <source>Thank you for asking about our classes, Have a nice day</source>
             <comment>Text</comment>
-            <translation type="unfinished">three eleven was a tough class. You will learn all about computer architecture and the assembly language!</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/CS312</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>C S three twelve was very interesting. You will learn about topics like digital circuits, memory, and central processing units.</source>
-            <comment>Text</comment>
-            <translation type="unfinished">C S three twelve was very interesting. You will learn about topics like digital circuits, memory, and central processing units.</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/CS325</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>Three twenty five helped me be able to write some great technical papers.</source>
-            <comment>Text</comment>
-            <translation type="unfinished">Three twenty five helped me be able to write some great technical papers.</translation>
+            <translation type="obsolete">Thank you for asking about our classes, Have a nice day</translation>
         </message>
     </context>
     <context>
@@ -100,10 +55,14 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>I didn't quite catch that. Please say the class again.</source>
             <comment>Text</comment>
-            <translation type="unfinished">I didn't quite catch that. Please say the class again.</translation>
+            <translation type="obsolete">I didn't quite catch that. Please say the class again.</translation>
+        </message>
+        <message>
+            <source>What other class would you like to know about?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What other class would you like to know about?</translation>
         </message>
     </context>
 </TS>
