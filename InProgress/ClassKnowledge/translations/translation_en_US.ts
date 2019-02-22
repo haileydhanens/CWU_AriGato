b25612c7b32c3a19b36889d7c12bdb4c4a82d11b
Matt Harker <mattsharker@gmail.com>
2019-02-22 15:07:59 -0800

    First Completed version of Class Knowledge
    This is the first working version of class knowledge. Contains a list of all classes and will give a description of the class.

--- InProgress/ClassKnowledge/translations/translation_en_US.ts
+++ InProgress/ClassKnowledge/translations/translation_en_US.ts
@@ -2,6 +2,15 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Ask again</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>What other class would you like to know about?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What other class would you like to know about?</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/CS110</name>
         <message>
             <source>Oh! C S one ten. That was there I learned all the basics. Good times back then.</source>
@@ -26,6 +35,19 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Exit speech</name>
+        <message>
+            <source>Thank you for asking about our classes! Have a nice day</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Thank you for asking about our classes! Have a nice day</translation>
+        </message>
+        <message>
+            <source>Thank you for asking about our classes, Have a nice day</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Thank you for asking about our classes, Have a nice day</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
@@ -33,10 +55,14 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>I didn't quite catch that. Please say the class again.</source>
             <comment>Text</comment>
-            <translation type="unfinished">I didn't quite catch that. Please say the class again.</translation>
+            <translation type="obsolete">I didn't quite catch that. Please say the class again.</translation>
+        </message>
+        <message>
+            <source>What other class would you like to know about?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What other class would you like to know about?</translation>
         </message>
     </context>
 </TS>
