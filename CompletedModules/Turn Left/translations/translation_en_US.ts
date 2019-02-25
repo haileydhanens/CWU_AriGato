c37de63fdfc961973148668063063aba5e26ec03
Matt Harker <mattsharker@gmail.com>
2019-02-25 13:07:50 -0800

    Added Turning Modules
    Added modules to have the robot turn left X degrees, right X degrees, and 180 degrees

---
+++ CompletedModules/Turn Left/translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>How far do you want me to turn?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">How far do you want me to turn?</translation>
+        </message>
+    </context>
+</TS>
